9c240ecf9d647948ac555be90f1e41edc0ff75b3
iendrus <iendr@DESKTOP-JFQEPNF>
2021-10-10 19:50:13 +0200

    Add project files.

---
+++ CRM/MenuService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM
+{
+    public class MenuService
+    {
+        private List<Menu> menuService { get; set; } = new List<Menu>();
+
+        public void AddNewMenu(int id, string name, string group)
+        {
+            Menu menu = new Menu() { Id = id, Name = name, Group = group };
+            menuService.Add(menu);
+        }
+
+        
+
+        public List<Menu> GetAllItems()
+        {
+            return menuService;
+        }
+
+
+
+    }
+
+
+
+
+}
